8a86808cd30b28b2d988d777108bf7728e174618
venkateshkunduru <venkateshkunduru123@gmail.com>
2019-04-22 19:43:18 -0500

    venki commited

--- controllers/bankorg.js
+++ controllers/bankorg.js
@@ -17,96 +17,83 @@ const notfoundstring = 'bank organisation not found'
 
 // GET all JSON
 api.get('/findall', (req, res) => {
-  res.setHeader('Content-Type', 'application/json')
-  const data = req.app.locals.bankorgs.query
-  res.send(JSON.stringify(data))
+  LOG.info(`Handling /findall ${req}`)
+  Model.find({}, (err, data) => {
+    res.json(data)
+  })
 })
 
 // GET one JSON by ID
 api.get('/findone/:id', (req, res) => {
-  res.setHeader('Content-Type', 'application/json')
+  LOG.info(`Handling /findone ${req}`)
   const id = parseInt(req.params.id)
-  const data = req.app.locals.bankorgs.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring) }
-  res.send(JSON.stringify(item))
+  Model.find({ _id: id }, (err, results) => {
+    if (err) { return res.end(notfoundstring) }
+    res.json(results[0])
+  })
 })
 
 // RESPOND WITH VIEWS  --------------------------------------------
 
 // GET to this controller base URI (the default)
 api.get('/', (req, res) => {
-  res.render('bankorg/index.ejs')
+  Model.find({},(err,data) => {
+    res.locals.bankorgs = data
+    res.render('bankorg/index.ejs')
+  })
 })
 
 // GET create
 api.get('/create', (req, res) => {
-  LOG.info(`Handling GET /create${req}`)
-  const item = new Model()
-  LOG.debug(JSON.stringify(item))
-  res.render('bankorg/create',
-    {
-      title: 'Create bank organisation',
-      layout: 'layout.ejs',
-      bankorg: item
-    })
+  LOG.info(`Handling GET /create ${req}`)
+  Model.find({}, (err, data) => {
+    res.locals.bankorgs = data
+    res.locals.bankorg = new Model()
+    res.render('bankorg/create')
+  })
 })
 
 // GET /delete/:id
 api.get('/delete/:id', (req, res) => {
   LOG.info(`Handling GET /delete/:id ${req}`)
   const id = parseInt(req.params.id)
-  const data = req.app.locals.bankorgs.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring) }
-  LOG.info(`RETURNING VIEW FOR ${JSON.stringify(item)}`)
-  return res.render('bankorg/delete.ejs',
-    {
-      title: 'Delete bank organisation',
-      layout: 'layout.ejs',
-      bankorg: item
-    })
+  Model.find({ _id: id }, (err, results) => {
+    if (err) { return res.end(notfoundstring) }
+    LOG.info(`RETURNING VIEW FOR ${JSON.stringify(results)}`)
+    res.locals.bankorg = results[0]
+    return res.render('bankorg/delete.ejs')
+  })
 })
 
 // GET /details/:id
 api.get('/details/:id', (req, res) => {
   LOG.info(`Handling GET /details/:id ${req}`)
   const id = parseInt(req.params.id)
-  const data = req.app.locals.bankorgs.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring) }
-  LOG.info(`RETURNING VIEW FOR ${JSON.stringify(item)}`)
-  return res.render('bankorg/details.ejs',
-    {
-      title: 'bank organisation Details',
-      layout: 'layout.ejs',
-      bankorg: item
-    })
+  Model.find({ _id: id }, (err, results) => {
+    if (err) { return res.end(notfoundstring) }
+    LOG.info(`RETURNING VIEW FOR ${JSON.stringify(results)}`)
+    res.locals.bankorg = results[0]
+    return res.render('bankorg/details.ejs')
+  })
 })
 
 // GET one
 api.get('/edit/:id', (req, res) => {
   LOG.info(`Handling GET /edit/:id ${req}`)
   const id = parseInt(req.params.id)
-  const data = req.app.locals.bankorgs.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring) }
-  LOG.info(`RETURNING VIEW FOR${JSON.stringify(item)}`)
-  return res.render('bankorg/edit.ejs',
-    {
-      title: 'bank organisation',
-      layout: 'layout.ejs',
-      bankorg: item
-    })
+  Model.find({ _id: id }, (err, results) => {
+    if (err) { return res.end(notfoundstring) }
+    LOG.info(`RETURNING VIEW FOR${JSON.stringify(results)}`)
+    res.locals.bankorg = results[0]
+    return res.render('bankorg/edit.ejs')
+  })
 })
-
 // HANDLE EXECUTE DATA MODIFICATION REQUESTS --------------------------------------------
 
 // POST new
 api.post('/save', (req, res) => {
   LOG.info(`Handling POST ${req}`)
   LOG.debug(JSON.stringify(req.body))
-  const data = req.app.locals.bankorgs.query
   const item = new Model()
   LOG.info(`NEW ID ${req.body._id}`)
   item._id = parseInt(req.body._id)
@@ -118,9 +105,11 @@ api.post('/save', (req, res) => {
   item.state = req.body.state
   item.zip = req.body.zip
   item.country = req.body.country
-  data.push(item)
-  LOG.info(`SAVING NEW bankorg ${JSON.stringify(item)}`)
-  return res.redirect('/bankorg')
+  item.save((err) => {
+    if (err) { return res.end('ERROR: bankorg could not be saved') }
+    LOG.info(`SAVING NEW bankorg ${JSON.stringify(item)}`)
+    return res.redirect('/bankorg')
+  })
 })
 
 // POST update with id
@@ -128,21 +117,26 @@ api.post('/save/:id', (req, res) => {
   LOG.info(`Handling SAVE request ${req}`)
   const id = parseInt(req.params.id)
   LOG.info(`Handling SAVING ID=${id}`)
-  const data = req.app.locals.bankorgs.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring) }
-  LOG.info(`ORIGINAL VALUES ${JSON.stringify(item)}`)
-  LOG.info(`UPDATED VALUES: ${JSON.stringify(req.body)}`)
-  item.email = req.body.email
-  item.name = req.body.name
-  item.street1 = req.body.street1
-  item.street2 = req.body.street2
-  item.city = req.body.city
-  item.state = req.body.state
-  item.zip = req.body.zip
-  item.country = req.body.country
-  LOG.info(`SAVING UPDATED bankorg ${JSON.stringify(item)}`)
-  return res.redirect('/bankorg')
+  Model.updateOne({ _id: id },
+    { // use mongoose field update operator $set
+      $set: {
+        email: req.body.email,
+        name: req.body.name,
+        street1: req.body.street1,
+        street2: req.body.street2,
+        city: req.body.city,
+        state: req.body.state,
+        zip: req.body.zip,
+        country: req.body.country
+      }
+    },
+    (err, item) => {
+      if (err) { return res.end(notfoundstring) }
+      LOG.info(`ORIGINAL VALUES ${JSON.stringify(item)}`)
+      LOG.info(`UPDATED VALUES: ${JSON.stringify(req.body)}`)
+      LOG.info(`SAVING UPDATED bankorg ${JSON.stringify(item)}`)
+      return res.redirect('/bankorg')
+    })
 })
 
 // DELETE id (uses HTML5 form method POST)
@@ -150,17 +144,11 @@ api.post('/delete/:id', (req, res) => {
   LOG.info(`Handling DELETE request ${req}`)
   const id = parseInt(req.params.id)
   LOG.info(`Handling REMOVING ID=${id}`)
-  const data = req.app.locals.bankorgs.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring) }
-  if (item.isActive) {
-    item.isActive = false
-    console.log(`Deacctivated item ${JSON.stringify(item)}`)
-  } else {
-    const item = remove(data, { _id: id })
-    console.log(`Permanently deleted item ${JSON.stringify(item)}`)
-  }
-  return res.redirect('/bankorg')
+  Model.remove({ _id: id }).setOptions({ single: true }).exec((err, deleted) => {
+    if (err) { return res.end(notfoundstring) }
+    console.log(`Permanently deleted item ${JSON.stringify(deleted)}`)
+    return res.redirect('/bankorg')
+  })
 })
 
 module.exports = api
