1c52a0b856df9681c7e6b5ff43e53dd84493c120
venkateshkunduru <venkateshkunduru123@gmail.com>
2019-04-22 20:34:31 -0500

    changing the database

--- controllers/category.js
+++ controllers/category.js
@@ -11,7 +11,7 @@ const LOG = require('../utils/logger.js')
 const find = require('lodash.find')
 const remove = require('lodash.remove')
 const Model = require('../models/category.js')
-const notfoundstring = 'category not found'
+const notfoundstring = 'bank organisation not found'
 
 // RESPOND WITH JSON DATA  --------------------------------------------
 
@@ -88,38 +88,43 @@ api.get('/edit/:id', (req, res) => {
     return res.render('category/edit.ejs')
   })
 })
-
 // HANDLE EXECUTE DATA MODIFICATION REQUESTS --------------------------------------------
 
 // POST new
 api.post('/save', (req, res) => {
   LOG.info(`Handling POST ${req}`)
   LOG.debug(JSON.stringify(req.body))
-  const data = req.app.locals.categorys.query
   const item = new Model()
   LOG.info(`NEW ID ${req.body._id}`)
   item._id = parseInt(req.body._id)
   item.categoryID = parseInt(req.body.categoryID)
   item.categoryName = (req.body.categoryName)
-  data.push(item)
-  LOG.info(`SAVING NEW category ${JSON.stringify(item)}`)
-  return res.redirect('/category')
+  item.save((err) => {
+    if (err) { return res.end('ERROR: category could not be saved') }
+    LOG.info(`SAVING NEW category ${JSON.stringify(item)}`)
+    return res.redirect('/category')
+  })
 })
 
-// POST update
+// POST update with id
 api.post('/save/:id', (req, res) => {
   LOG.info(`Handling SAVE request ${req}`)
   const id = parseInt(req.params.id)
   LOG.info(`Handling SAVING ID=${id}`)
-  const data = req.app.locals.categorys.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring) }
-  LOG.info(`ORIGINAL VALUES ${JSON.stringify(item)}`)
-  LOG.info(`UPDATED VALUES: ${JSON.stringify(req.body)}`)
-  item.categoryID = parseInt(req.body.categoryID)
-  item.categoryName = (req.body.categoryName)
-  LOG.info(`SAVING UPDATED category ${JSON.stringify(item)}`)
-  return res.redirect('/category')
+  Model.updateOne({ _id: id },
+    { // use mongoose field update operator $set
+      $set: {
+        categoryId: req.body.categoryId,
+        categoryName: req.body.categoryName
+      }
+    },
+    (err, item) => {
+      if (err) { return res.end(notfoundstring) }
+      LOG.info(`ORIGINAL VALUES ${JSON.stringify(item)}`)
+      LOG.info(`UPDATED VALUES: ${JSON.stringify(req.body)}`)
+      LOG.info(`SAVING UPDATED category ${JSON.stringify(item)}`)
+      return res.redirect('/category')
+    })
 })
 
 // DELETE id (uses HTML5 form method POST)
@@ -127,17 +132,11 @@ api.post('/delete/:id', (req, res) => {
   LOG.info(`Handling DELETE request ${req}`)
   const id = parseInt(req.params.id)
   LOG.info(`Handling REMOVING ID=${id}`)
-  const data = req.app.locals.categorys.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring) }
-  if (item.isActive) {
-    item.isActive = false
-    console.log(`Deacctivated item ${JSON.stringify(item)}`)
-  } else {
-    const item = remove(data, { _id: id })
-    console.log(`Permanently deleted item ${JSON.stringify(item)}`)
-  }
-  return res.redirect('/category')
+  Model.remove({ _id: id }).setOptions({ single: true }).exec((err, deleted) => {
+    if (err) { return res.end(notfoundstring) }
+    console.log(`Permanently deleted item ${JSON.stringify(deleted)}`)
+    return res.redirect('/category')
+  })
 })
 
 module.exports = api
--- controllers/transaction.js
+++ controllers/transaction.js
@@ -16,87 +16,76 @@ const notfoundstring = 'transaction not found'
 
 // GET all JSON
 api.get('/findall', (req, res) => {
-  res.setHeader('Content-Type', 'application/json')
-  const data = req.app.locals.transactions.query
-  res.send(JSON.stringify(data))
+  LOG.info(`Handling /findall ${req}`)
+  Model.find({}, (err, data) => {
+    res.json(data)
+  })
 })
 
 // GET one JSON by ID
 api.get('/findone/:id', (req, res) => {
-  res.setHeader('Content-Type', 'application/json')
+  LOG.info(`Handling /findone ${req}`)
   const id = parseInt(req.params.id)
-  const data = req.app.locals.transactions.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring) }
-  res.send(JSON.stringify(item))
+  Model.find({ _id: id }, (err, results) => {
+    if (err) { return res.end(notfoundstring) }
+    res.json(results[0])
+  })
 })
 
 // RESPOND WITH VIEWS  --------------------------------------------
 
 // GET to this controller base URI (the default)
 api.get('/', (req, res) => {
-  res.render('transaction/index.ejs')
+  Model.find({},(err,data) => {
+    res.locals.transactions = data
+    res.render('transaction/index.ejs')
+  })
 })
 
 // GET create
 api.get('/create', (req, res) => {
-  LOG.info(`Handling GET /create${req}`)
-  const item = new Model()
-  LOG.debug(JSON.stringify(item))
-  res.render('transaction/create',
-    {
-      title: 'Create transaction',
-      layout: 'layout.ejs',
-      transaction: item
-    })
+  LOG.info(`Handling GET /create ${req}`)
+  Model.find({}, (err, data) => {
+    res.locals.transactions = data
+    res.locals.transaction = new Model()
+    res.render('transaction/create')
+  })
 })
 
 // GET /delete/:id
 api.get('/delete/:id', (req, res) => {
   LOG.info(`Handling GET /delete/:id ${req}`)
   const id = parseInt(req.params.id)
-  const data = req.app.locals.transactions.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring) }
-  LOG.info(`RETURNING VIEW FOR ${JSON.stringify(item)}`)
-  return res.render('transaction/delete.ejs',
-    {
-      title: 'Delete transaction',
-      layout: 'layout.ejs',
-      transaction: item
-    })
+  Model.find({ _id: id }, (err, results) => {
+    if (err) { return res.end(notfoundstring) }
+    LOG.info(`RETURNING VIEW FOR ${JSON.stringify(results)}`)
+    res.locals.transaction = results[0]
+    return res.render('transaction/delete.ejs')
+  })
 })
 
 // GET /details/:id
 api.get('/details/:id', (req, res) => {
   LOG.info(`Handling GET /details/:id ${req}`)
   const id = parseInt(req.params.id)
-  const data = req.app.locals.transactions.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring) }
-  LOG.info(`RETURNING VIEW FOR ${JSON.stringify(item)}`)
-  return res.render('transaction/details.ejs',
-    {
-      title: 'transaction Details',
-      layout: 'layout.ejs',
-      transaction: item
-    })
+  Model.find({ _id: id }, (err, results) => {
+    if (err) { return res.end(notfoundstring) }
+    LOG.info(`RETURNING VIEW FOR ${JSON.stringify(results)}`)
+    res.locals.transaction = results[0]
+    return res.render('transaction/details.ejs')
+  })
 })
 
 // GET one
 api.get('/edit/:id', (req, res) => {
   LOG.info(`Handling GET /edit/:id ${req}`)
   const id = parseInt(req.params.id)
-  const data = req.app.locals.transactions.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring) }
-  LOG.info(`RETURNING VIEW FOR${JSON.stringify(item)}`)
-  return res.render('transaction/edit.ejs',
-    {
-      title: 'transactions',
-      layout: 'layout.ejs',
-      transaction: item
-    })
+  Model.find({ _id: id }, (err, results) => {
+    if (err) { return res.end(notfoundstring) }
+    LOG.info(`RETURNING VIEW FOR${JSON.stringify(results)}`)
+    res.locals.transaction = results[0]
+    return res.render('transaction/edit.ejs')
+  })
 })
 
 // HANDLE EXECUTE DATA MODIFICATION REQUESTS --------------------------------------------
@@ -105,51 +94,51 @@ api.get('/edit/:id', (req, res) => {
 api.post('/save', (req, res) => {
   LOG.info(`Handling POST ${req}`)
   LOG.debug(JSON.stringify(req.body))
-  const data = req.app.locals.transactions.query
   const item = new Model()
   LOG.info(`NEW ID ${req.body._id}`)
-  item._id = parseInt(req.body._id)
   item.transactionDate = req.body.transactionDate
   item.transactionType = req.body.transactionType
   item.transactionAmount = parseFloat(req.body.transactionAmount)
-  data.push(item)
-  LOG.info(`SAVING NEW transaction ${JSON.stringify(item)}`)
-  return res.redirect('/transaction')
+  item.save((err) => {
+    if (err) { return res.end('ERROR: transaction could not be saved') }
+    LOG.info(`SAVING NEW transaction ${JSON.stringify(item)}`)
+    return res.redirect('/transaction')
+  })
 })
 
-// POST update
+// POST update with id
 api.post('/save/:id', (req, res) => {
   LOG.info(`Handling SAVE request ${req}`)
   const id = parseInt(req.params.id)
   LOG.info(`Handling SAVING ID=${id}`)
-  const data = req.app.locals.transactions.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring) }
-  LOG.info(`ORIGINAL VALUES ${JSON.stringify(item)}`)
-  LOG.info(`UPDATED VALUES: ${JSON.stringify(req.body)}`)
-  item.transactionDate = req.body.transactionDate
-  item.transactionType = req.body.transactionType
-  item.transactionAmount = parseFloat(req.body.transactionAmount)
-  LOG.info(`SAVING UPDATED transaction ${JSON.stringify(item)}`)
-  return res.redirect('/transaction')
+  Model.updateOne({ _id: id },
+    { // use mongoose field update operator $set
+      $set: {
+        transactionDate: req.body.transactionDate,
+        transactionType: req.body.transactionType,
+        transactionAmount: req.body.transactionAmount
+      }
+    },
+    (err, item) => {
+      if (err) { return res.end(notfoundstring) }
+      LOG.info(`ORIGINAL VALUES ${JSON.stringify(item)}`)
+      LOG.info(`UPDATED VALUES: ${JSON.stringify(req.body)}`)
+      LOG.info(`SAVING UPDATED transaction ${JSON.stringify(item)}`)
+      return res.redirect('/transaction')
+    })
 })
+
 
 // DELETE id (uses HTML5 form method POST)
 api.post('/delete/:id', (req, res) => {
   LOG.info(`Handling DELETE request ${req}`)
   const id = parseInt(req.params.id)
   LOG.info(`Handling REMOVING ID=${id}`)
-  const data = req.app.locals.transactions.query
-  const item = find(data, { _id: id })
-  if (!item) { return res.end(notfoundstring) }
-  if (item.isActive) {
-    item.isActive = false
-    console.log(`Deacctivated item ${JSON.stringify(item)}`)
-  } else {
-    const item = remove(data, { _id: id })
-    console.log(`Permanently deleted item ${JSON.stringify(item)}`)
-  }
-  return res.redirect('/transaction')
+  Model.remove({ _id: id }).setOptions({ single: true }).exec((err, deleted) => {
+    if (err) { return res.end(notfoundstring) }
+    console.log(`Permanently deleted item ${JSON.stringify(deleted)}`)
+    return res.redirect('/transaction')
+  })
 })
 
 module.exports = api
